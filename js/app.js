4fee2a807c755fa11148e12b8c5f085992536a4d
NTIAliceB <71005674+NTIAliceB@users.noreply.github.com>
2020-11-06 13:42:00 +0100

    d

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-Math.random(6)
+function randomDice() {
 
-document.addEventListener("click", function(){
-    document.getElementById("demo").innerHTML = "Hello World";
-  });
-
-  function myFunction(p1, p2) {
-    return p1 * p2; 
-  }
-
-  document.getElementById("myImg").src = "hackanm.gif";
+}
